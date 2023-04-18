014149b7c4c444fe5e8a5a5056951499227429f1
Karankevich <liza_minyaeva@mail.ru>
2023-04-18 17:14:42 +0300

    Реализация методов класса Item (получение общей стоимости и применение скидки) + тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+@pytest.fixture
+def class_example_fixture():
+    return Item("Монитор", 15000, 6)
+def test_calculate_total_price(class_example_fixture):
+    assert class_example_fixture.calculate_total_price() == 90000
+
